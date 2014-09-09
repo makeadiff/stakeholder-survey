ad829dfe9ed92e343b4e4f25bc45afa424792111
Binny V A <binnyva@gmail.com>
2014-09-09 18:58:25 +0530

    Removed the DB connection part from the 'configuration.php' file

--- configuration.php
+++ configuration.php
@@ -5,8 +5,4 @@ $config = array(
 	'site_title'	=> 'Stakeholder Survey',
 	'site_url'		=> 'http://'.$server_host.'/Sites/community/makeadiff/makeadiff.in/',
 	'site_home'		=> 'http://'.$server_host.'/Sites/community/makeadiff/makeadiff.in/apps/stakeholder-survey/',
-	'db_host'		=> 'localhost', 
-	'db_user'		=> 'root',
-	'db_password'	=> '',
-	'db_database'	=> 'Project_Madapp',
 );
