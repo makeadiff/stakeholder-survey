28185276b7dfc3252985f702cd107bf6c9d08409
Binny V A <binnyva@gmail.com>
2015-09-28 18:49:06 +0530

    Fixed an issue that continued to show the form even with enetered data

--- 360_admin/questions.php
+++ 360_admin/questions.php
@@ -4,7 +4,9 @@ require('./common.php');
 $page = new Crud('prism_questions');
 
 $page->setListingFields('subject');
-$page->setFormFields('subject');
+//$page->setFormFields('subject');
+$page->allow['save_and_edit_form_button'] = false;
+$page->allow['save_and_new_form_button'] = false;
 $page->allow['delete'] = false;
 $page->allow['add'] = false;
 render();
