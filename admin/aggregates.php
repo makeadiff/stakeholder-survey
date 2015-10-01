5fe6e12257670bc0e70385a3d9be5e98b2d3e411
Binny V A <binnyva@gmail.com>
2015-10-01 13:16:40 +0530

    Changed from cycle system to survey event

--- admin/aggregates.php
+++ admin/aggregates.php
@@ -6,16 +6,20 @@ $page->title = "Aggregates";
 
 $city_id = 0;
 $vertical_id = 1;
+$survey_event_id = $sql->getOne("SELECT MAX(id) FROM SS_Survey_Event"); //get_cycle();
 
-if(isset($QUERY['cycle'])) $cycle = $QUERY['cycle'];
-else $cycle = get_cycle();
-$page->code['top'] = '<form action="" method="post"><label for="cycle">Cycle</label><select name="cycle">
-<option value="1"' . (($cycle == 1) ? 'selected':'') . '>Cycle 1</option>
-<option value="2"' . (($cycle == 2) ? 'selected':'') . '>Cycle 2</option>
-<option value="3"' . (($cycle == 3) ? 'selected':'') . '>Cycle 3</option>
-<option value="4"' . (($cycle == 4) ? 'selected':'') . '>Cycle 4</option>
-<option value="5"' . (($cycle == 5) ? 'selected':'') . '>Cycle 5</option>
-</select><input name="action" type="submit" value="Change" /></form>';
+if(isset($QUERY['survey_event_id'])) $survey_event_id = $QUERY['survey_event_id'];
+
+
+$all_survey_events = $sql->getById("SELECT id, CONCAT(id, ') ', DATE_FORMAT(added_on, '%Y %M')) AS name FROM SS_Survey_Event");
+
+$page->code['top'] = '<form action="" method="post"><label for="survey_event_id">Survey Event</label><select name="survey_event_id">';
+foreach ($all_survey_events as $id => $name) {
+	$sel = '';
+	if($id == $survey_event_id) $sel = 'selected="selected"';
+	$page->code['top'] .= "<option value='$id' $sel>$name</option>";
+}
+$page->code['top'] .= '</select><input name="action" type="submit" value="Change" /></form>';
 
 $all_answer = array(1=>'Level 1', 3=>'Level 3', 5=>'Level 5');
 //$all_questions = $sql->getById("SELECT id,question FROM SS_Question WHERE status='1'");
@@ -29,7 +33,7 @@ foreach ($all_answer as $answer_number => $answer_text) {
 	$page->addField('answer_'.$answer_number, $answer_text, 'virtual', array(), array(
 		'sql'=>"SELECT COUNT(DISTINCT U.id) FROM SS_UserAnswer UA
 					INNER JOIN User U ON U.id=UA.user_id
-					WHERE $where U.status='1' AND U.user_type='volunteer' AND UA.survey_event_id=$cycle
+					WHERE $where U.status='1' AND U.user_type='volunteer' AND UA.survey_event_id=$survey_event_id
 					AND UA.answer='$answer_number' AND UA.question_id='%id%'"));
 }
 
